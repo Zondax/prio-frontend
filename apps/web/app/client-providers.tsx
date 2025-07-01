68e254cf58d7f20e7c4ebd0b2f808709df559d3e
Juan Leni <juan.leni@zondax.ch>
2025-07-01 22:53:19 +0200

    style: format client-providers component

--- apps/web/app/client-providers.tsx
+++ apps/web/app/client-providers.tsx
@@ -4,9 +4,5 @@ import { ProviderStack, webProviders } from '@zondax/ui-common/client'
 import type { ReactNode } from 'react'
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
-  return (
-    <ProviderStack providers={webProviders}>
-      {children}
-    </ProviderStack>
-  )
+  return <ProviderStack providers={webProviders}>{children}</ProviderStack>
 }
